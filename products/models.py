96bf2b59e859d0a32fd0ca446a26fe45f68f0b81
tkslcc <60019174+tkslcc@users.noreply.github.com>
2021-07-23 08:07:41 -0600

    Added orders model and updated settings to add orders to installed_apps

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+# MIGRATE WHEN YOU CHANGE THE MODEL
+
 # Create your models here.
 class Product(models.Model):
     #id = models.AutoField()
@@ -11,6 +13,7 @@ class Product(models.Model):
     content = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     inventory = models.IntegerField(default=0)
+    featured = models.BooleanField(default=False)
 
 def has_inventory(self):
     return self.inventory > 0
